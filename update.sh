6cdf5f53bac2986bd29dfd85173f08276db46a88
emile <emile@vauge.com>
2016-02-24 13:09:43 +0100

    initial commit
    Signed-off-by: emile <emile@vauge.com>

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 0 ] ; then
+	echo "Usage: ./update.sh <traefik tag or branch>"
+	exit
+fi
+
+VERSION=$1
+
+# cd to the current directory so the script can be run from anywhere.
+cd `dirname $0`
+
+# Update the certificates.
+echo "Updating certificates..."
+./certs/update.sh
+
+echo "Fetching and building traefik $VERSION..."
+wget -O traefik https://github.com/emilevauge/traefik/releases/download/$VERSION/traefik
+chmod +x traefik
+
+echo "Done."
