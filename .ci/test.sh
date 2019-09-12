342f566ce86a3bd8ded4ca95ce048200e8cdd63a
Fernandez Ludovic <ludovic@containo.us>
2019-09-12 19:49:34 +0200

    chore: refactor tests script.

---
+++ .ci/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+# set -x
+
+VERSION=$(cat version)
+
+API_OPTS=""
+if [[ ${VERSION} = v2* ]]; then
+    API_OPTS="--api.insecure"
+fi
+
+TARGETS=(
+    "traefik:${VERSION}-alpine!./alpine"
+    "traefik:${VERSION}!./scratch"
+)
+
+for target in ${TARGETS[@]}; do
+    image="${target%%!*}"
+    path="${target##*!}"
+    
+    docker build -t "${image}" "${path}"
+    # Docker Official Library Tests
+    ~/official-images/test/run.sh "$image"
+    # Smoke Test of the image
+    docker run --name lb -d -p 8080:8080 "${image}" --api "${API_OPTS}"
+    sleep 2
+    docker ps
+    curl --verbose --fail http://localhost:8080
+    docker rm -f lb
+done
